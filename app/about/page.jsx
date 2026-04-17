58ba2d016c00df874165d6d27a96a1b41a2ce050
Charlie Pariente <charlesmapariente@gmail.com>
2026-04-16 17:15:05 -0700

    Update about page with short bio about the band

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,30 +1,31 @@
 "use client";
 
 import Link from "next/link";
-import Button from "./components/Utils/Button";
-import downDayPic from "../public/Pics/down day artwork.jpg";
-import VideoCard from "./components/VideoCard";
-import ImageCard from "./components/ImageCard";
-import PageSection from "./components/Utils/PageSection";
+import Button from "../components/Utils/Button";
+import downDayPic from "../../public/Pics/down day artwork.jpg";
+import ImageCard from "../components/ImageCard";
+import PageSection from "../components/Utils/PageSection";
 
-export default function FrontPage() {
+export default function AboutPage() {
   return (
-    <div className="front-page-container">
-      <PageSection className="fp-description">
-        <p className="fp-paragraph">
-          From the sun-soaked city of Belize to the foggy streets of San
-          Francisco, cmap’s journey echoes in every riff and rhythm, igniting
-          listeners with an electrifying arsenal of musical prowess. The
-          experience seamlessly blends rock, punk, and Caribbean influences into
-          an explosive sonic cocktail. Get ready to ride the wave of sound that
-          defines the new era of modern Belizean rock.
-        </p>
+    <div className="about-page-container">
+      <PageSection className="ap-section">
+        <div className="ap-background">
+          <h2 className="ap-bio">
+            From the sun-soaked city of Belize to the foggy streets of San
+            Francisco, cmap’s journey echoes in every riff and rhythm, igniting
+            listeners with an electrifying arsenal of musical prowess. The
+            experience seamlessly blends rock, punk, and Caribbean influences into
+            an explosive sonic cocktail. Get ready to ride the wave of sound that
+            defines the new era of modern Belizean rock.
+          </h2>
+        </div>
       </PageSection>
       <PageSection className="fp-featured-image">
         <ImageCard
           src={downDayPic}
           className="fp-msg"
-          msg="The new single Down Day is out now!"
+          msg="Check out the new single today!"
         />
         <Link
           href="https://distrokid.com/hyperfollow/cmap/down-day"
@@ -32,16 +33,6 @@ export default function FrontPage() {
         >
           <Button className="fp-btn" msg="Stream Here" />
         </Link>
-      </PageSection>
-      <PageSection className="fp-featured-video">
-        <VideoCard
-          youtubeId={"mzmf3YeukF0"}
-          title="Down Day Official Audio"
-          vidTitleClass="fp-vid-title"
-        />
-      </PageSection>
-      <PageSection>
-        <form action=""></form>
       </PageSection>
     </div>
   );
