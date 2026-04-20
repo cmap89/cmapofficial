cdfcb9e6fe2c02073e97c1870fbdbc153a530d75
Charlie Pariente <charlesmapariente@gmail.com>
2026-04-20 15:58:56 -0700

    Update services page to include contact form

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,14 +1,13 @@
 'use client';
 
 import PageSection from "../components/Utils/PageSection";
+import FormCard from "../components/FormCard";
 
 export default function BookPage() {
   return (
     <div className="services-page-container">
-      <PageSection className="service-desc-sect" >
+      <PageSection className="skill-list-sect" >
         <h2>With almost 30 years of music experience and training, cmap offers many ways to bring your music to life.</h2>
-      </PageSection>
-      <PageSection className="skill-list-sect">
         <ul className="skill-list">
           <li className="skill-item">
             <h2>Audio Engineering</h2>
@@ -23,6 +22,10 @@ export default function BookPage() {
             <p>One-on-one instruction for all skill levels</p>
           </li>
         </ul>
+      </PageSection>
+      <PageSection className="sp-contact-sect" >
+        <h2 className="sp-contact-heading">Contact me today</h2>
+        <FormCard scriptSrc={"https://app.mymusicstaff.com/Widget/v4/Widget.ashx?settings=eyJTY2hvb2xJRCI6InNjaF9QenBKViIsIldlYnNpdGVJRCI6Indic19WMDRKeCIsIldlYnNpdGVCbG9ja0lEIjoid2JiX3pnZzdESm4ifQ=="} title={'text 1'} description={'txt2'} ></FormCard>
       </PageSection>
     </div>
   );
