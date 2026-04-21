761f00f5e0c40242355682bba4bcba8e9e0d98e8
Charlie Pariente <charlesmapariente@gmail.com>
2026-04-21 16:28:37 -0700

    Update about page responsive styles

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -19,6 +19,7 @@ export default function AboutPage() {
               Through my work, I combine technical expertise with creative intuition to help bring ideas to life. As an educator, I&apos;m passionate about sharing knowledge in a way that&apos;s clear, practical, and inspiring. I&apos;m actively working on new music and collaborations, and I&apos;m always looking to connect with artists and projects where I can contribute as an engineer, performer, songwriter, and producer.
             </p>
           </div>
+          <br />
           <Link className="ap-service-link" href="/services" >
             <h2>Want to get in touch?</h2>
           </Link>
